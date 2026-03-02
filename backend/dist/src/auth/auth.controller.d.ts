67f33f09e02eae7c817e851b439385fe63a95135
metiinie <abumahilkerim@gmail.com>
2026-03-02 23:20:22 +0300

    update home

--- backend/dist/src/auth/auth.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { AuthService } from './auth.service';
-export declare class AuthController {
-    private authService;
-    constructor(authService: AuthService);
-    getProfile(req: any): any;
-}
